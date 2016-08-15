72b8468ad68a2ec87f0a11cd9beea344ea015755
ongubo <roraffmails5@gmail.com>
2016-08-16 00:18:56 +0300

    Create schema for weather database. add collection for weather data

--- config/database.php
+++ config/database.php
@@ -49,9 +49,9 @@ return [
             'driver'   => 'mongodb',
             'host'     => env('DB_HOST', 'localhost'),
             'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'database'  => env('DB_DATABASE', 'weather'),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
             'options'  => [
                 'database' => 'admin', // sets the authentication database required by mongo 3
             ],
